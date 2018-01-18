773665737d7ebfd0c9f9d0b461025a347d173a9d
yangyq02 <yangyq02@yschome.com>
2018-01-18 15:34:31 +0800

    添加PDF打印例子

---
+++ src/com/evangelsoft/pdf/DataUtil.java
@@ -0,0 +1,20 @@
+package com.evangelsoft.pdf;
+
+import org.apache.commons.lang.StringUtils;
+
+public class DataUtil {
+
+	/**
+	* 分割路径
+	* @param path
+	* @return 返回分割后的路径
+	*/
+	public static String[] separatePath(String path) {
+		if (StringUtils.isBlank(path)) {
+			return null;
+		}
+		String[] sep = path.split("\\.");
+		return new String[] { sep[0], sep[1] };
+	}
+
+}
